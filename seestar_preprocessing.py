e28216d3ca8c2d5301501a6295b8ea46d14532ef
Nazmus Nasir <nazmus.nasir@gmail.com>
2025-05-15 00:47:06 -0400

    Refactored logs

--- seestar_preprocessing.py
+++ seestar_preprocessing.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 import os
 import sys
 import shutil
 import tkinter as tk
 from tkinter import ttk
 import sirilpy as s
-from sirilpy import NoImageError, tksiril
+from sirilpy import LogColor, NoImageError, tksiril
 from ttkthemes import ThemedTk
 from astropy.io import fits
 
@@ -14,6 +15,7 @@ APP_NAME = "Smart Telescope Preprocessing"
 VERSION = "1.0.0"
 AUTHOR = "Nazmus Nasir"
 WEBSITE = "https://www.Naztronomy.com (https://www.YouTube.com/Naztronomy)"
+
 
 class PreprocessingInterface:
     def __init__(self, root):
@@ -25,7 +27,7 @@ class PreprocessingInterface:
         self.style = tksiril.standard_style()
 
         self.siril = s.SirilInterface()
-        
+
         # Flags for mosaic mode and drizzle status
         # If mosaic mode, fitseq will be used but cannot use framing max
         # if drizzle is off, images will be debayered on convert
@@ -33,9 +35,9 @@ class PreprocessingInterface:
         self.drizzle_status = False
         try:
             self.siril.connect()
-            self.siril.log("Connected to Siril", self.siril.LogColor.GREEN)
+            self.siril.log("Connected to Siril", LogColor.GREEN)
         except s.SirilConnectionError:
-            self.siril.log("Failed to connect to Siril", self.siril.LogColor.RED)   
+            self.siril.log("Failed to connect to Siril", LogColor.RED)
             self.close_dialog()
 
         try:
@@ -47,7 +49,11 @@ class PreprocessingInterface:
         self.gaia_catalogue_available = False
         try:
             catalog_status = self.siril.get_siril_config("core", "catalogue_gaia_astro")
-            if catalog_status and catalog_status != "(not set)" and os.path.isfile(catalog_status):
+            if (
+                catalog_status
+                and catalog_status != "(not set)"
+                and os.path.isfile(catalog_status)
+            ):
                 self.gaia_catalogue_available = True
 
         except s.CommandError:
@@ -55,40 +61,65 @@ class PreprocessingInterface:
         self.current_working_directory = self.siril.get_siril_wd()
         lights_directory = os.path.join(self.current_working_directory, "lights")
         if not os.path.isdir(lights_directory):
-            raise self.siril.error_messagebox("Directory 'lights' does not exist, please change current working directory and try again.", True)
+            raise self.siril.error_messagebox(
+                "Directory 'lights' does not exist, please change current working directory and try again.",
+                True,
+            )
         self.create_widgets()
-
 
     # Dirname: lights, darks, biases, flats
     def convert_files(self, dir_name):
         directory = os.path.join(self.current_working_directory, dir_name)
         if os.path.isdir(directory):
             self.siril.cmd("cd", dir_name)
-            file_count = len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
+            file_count = len(
+                [
+                    name
+                    for name in os.listdir(directory)
+                    if os.path.isfile(os.path.join(directory, name))
+                ]
+            )
             if file_count > 2048:
-                self.siril.error_messagebox("More than 2048 images found. Please select '2048+ Mode' and try again.")
+                self.siril.error_messagebox(
+                    "More than 2048 images found. Please select '2048+ Mode' and try again."
+                )
                 self.close_dialog()
 
             try:
                 if self.fitseq_mode:
                     if not self.drizzle_status:
-                        self.siril.cmd("convert", dir_name, "-fitseq", "-debayer", "-out=../process")
+                        self.siril.cmd(
+                            "convert",
+                            dir_name,
+                            "-fitseq",
+                            "-debayer",
+                            "-out=../process",
+                        )
                     else:
-                        self.siril.cmd("convert", dir_name, "-fitseq", "-out=../process")
+                        self.siril.cmd(
+                            "convert", dir_name, "-fitseq", "-out=../process"
+                        )
                 else:
                     if not self.drizzle_status:
-                        self.siril.cmd("convert", dir_name, "-debayer", "-out=../process")
+                        self.siril.cmd(
+                            "convert", dir_name, "-debayer", "-out=../process"
+                        )
                     else:
                         self.siril.cmd("convert", dir_name, "-out=../process")
             except s.CommandError as e:
                 self.siril.error_messagebox(f"{e}")
                 self.close_dialog()
             self.siril.cmd("cd", "../process")
-            self.siril.log(f"Converted {file_count} files for processing!", self.siril.LogColor.GREEN)
+            self.siril.log(
+                f"Converted {file_count} files for processing!", LogColor.GREEN
+            )
         else:
             self.siril.error_messagebox(f"Directory {directory} does not exist", True)
-            raise NoImageError((f"No directory named \"{dir_name}\" at this location. Make sure the working directory is correct."))
-            
+            raise NoImageError(
+                (
+                    f'No directory named "{dir_name}" at this location. Make sure the working directory is correct.'
+                )
+            )
 
     # Plate solve on sequence runs when file count < 2048
     def seq_plate_solve(self, seq_name):
@@ -96,7 +127,7 @@ class PreprocessingInterface:
         self.siril.cmd(
             "seqplatesolve", seq_name, "-nocache", "-force", "-disto=ps_distortion"
         )
-        self.siril.log(f"Platesolved {seq_name}", self.siril.LogColor.GREEN)
+        self.siril.log(f"Platesolved {seq_name}", LogColor.GREEN)
 
     def seq_graxpert_bg_extract(self):
         """Runs Graxpert to extract bg for each file in the sequence. Uses GPU if available."""
@@ -105,18 +136,13 @@ class PreprocessingInterface:
     def seq_bg_extract(self):
         """Runs the siril command 'seqsubsky' to extract the background from the plate solved files."""
         self.siril.cmd("seqsubsky", "lights_", "1", "-samples=10")
-        self.siril.log("Background extracted from Sequence", self.siril.LogColor.GREEN)
+        self.siril.log("Background extracted from Sequence", LogColor.GREEN)
 
     def seq_apply_reg(self, seq_name, drizzle_amount, pixel_fraction):
-        """Apply Existing Registration to the sequence. """
+        """Apply Existing Registration to the sequence."""
         if self.fitseq_mode:
-            cmd_args = [
-                "register",
-                seq_name,
-                "-2pass",
-                "-selected"
-            ]
-            
+            cmd_args = ["register", seq_name, "-2pass", "-selected"]
+
         else:
             cmd_args = [
                 "seqapplyreg",
@@ -133,9 +159,9 @@ class PreprocessingInterface:
 
         # Need the extra reg for fitseq
         if self.fitseq_mode:
-            self.siril.cmd("seqapplyreg", seq_name,"-filter-round=2.5k")
+            self.siril.cmd("seqapplyreg", seq_name, "-filter-round=2.5k")
 
-        self.siril.log("Registered Sequence", self.siril.LogColor.GREEN)
+        self.siril.log("Registered Sequence", LogColor.GREEN)
 
     def seq_stack(self, seq_name, feather, feather_amount):
         """Stack it all, and feather if it's provided"""
@@ -152,16 +178,17 @@ class PreprocessingInterface:
             cmd_args.append(f"-feather={feather_amount}")
         self.siril.cmd(*cmd_args)
 
-        self.siril.log("Complete stack!", self.siril.LogColor.GREEN)
+        self.siril.log("Completed stacking!", LogColor.GREEN)
 
     def save_image(self, suffix):
         """Saves the image as a FITS file."""
-        file_name = f"$OBJECT:%s$_$STACKCNT:%d$x$EXPTIME:%d$sec_$DATE-OBS:dt${suffix}"
+        current_datetime = datetime.now().strftime("%Y%m%d_%H%M")
+        file_name = f"$OBJECT:%s$_$STACKCNT:%d$x$EXPTIME:%d$sec_$DATE-OBS:dt${current_datetime}{suffix}"
         self.siril.cmd(
             "save",
             f"../{file_name}",
         )
-        self.siril.log(f"Saved file: {file_name}", self.siril.LogColor.GREEN)
+        self.siril.log(f"Saved file: {file_name}", LogColor.GREEN)
 
     def load_registered_image(self):
         """Loads the registered image. Currently unused"""
@@ -171,9 +198,15 @@ class PreprocessingInterface:
     def image_plate_solve(self):
         """Plate solve the loaded image with the '-force' argument."""
         self.siril.cmd("platesolve", "-force")
-        self.siril.log("Platesolved image", self.siril.LogColor.GREEN)
+        self.siril.log("Platesolved image", LogColor.GREEN)
 
-    def spcc(self, oscsensor="ZWO Seestar S50", filter="broadband", catalog="localgaia", whiteref="Average Spiral Galaxy"):
+    def spcc(
+        self,
+        oscsensor="ZWO Seestar S50",
+        filter="broadband",
+        catalog="localgaia",
+        whiteref="Average Spiral Galaxy",
+    ):
         """SPCC with oscsensor, filter, catalog, and whiteref."""
 
         args = [
@@ -181,27 +214,15 @@ class PreprocessingInterface:
             f"-catalog={catalog}",
             f"-whiteref={whiteref}",
         ]
-        
+
         if oscsensor in {"ZWO Seestar S30", "ZWO Seestar S50"}:
             if filter == "broadband":
-                args.extend(
-                    [
-                        "-oscfilter=UV/IR Block",
-                    ]
-                )
+                args.append("-oscfilter=UV/IR Block")
             elif filter == "narrowband":
-                args.extend(
-                    [
-                        "-oscfilter=ZWO Seestar LP",
-                    ]
-                )
+                args.append("-oscfilter=ZWO Seestar LP")
         elif oscsensor == "Dwarf 3":
             if filter == "broadband":
-                args.extend(
-                    [
-                        "-oscfilter=Sony IMX678",
-                    ]
-                )
+                args.append("-oscfilter=Sony IMX678")
             elif filter == "narrowband":
                 args.extend(
                     [
@@ -215,40 +236,36 @@ class PreprocessingInterface:
                     ]
                 )
         else:
-            args.extend(
-                    [
-                        "-oscfilter=UV/IR Block",
-                    ]
-                )
-            
+            args.append("-oscfilter=UV/IR Block")
+
         # Need to put double quotes around each argument, otherwise the spaces cause issues
-        quoted_args = [f'"{arg}"' for arg in args]        
+        quoted_args = [f'"{arg}"' for arg in args]
         self.siril.cmd("spcc", *quoted_args)
 
         self.save_image("_spcc")
 
-        self.siril.log("SPCC'd image", self.siril.LogColor.GREEN)
+        self.siril.log("SPCC'd image", LogColor.GREEN)
 
     def load_image(self, image_name):
         """Loads the result."""
         self.siril.cmd("load", image_name)
-        self.siril.log(f"Loaded image: {image_name}", self.siril.LogColor.GREEN)
+        self.siril.log(f"Loaded image: {image_name}", LogColor.GREEN)
 
-    def autostretch(self):
+    def autostretch(self, do_spcc):
         """Autostretch as a way to preview the final result"""
-        self.siril.cmd("autostretch")
-        self.siril.log("Autostretched image. You may want to open the _spcc file instead", self.siril.LogColor.GREEN)
+        self.siril.cmd("autostretch", *(["-linked"] if do_spcc else []))
+        self.siril.log(
+            "Autostretched image." + (" You may want to open the _spcc file instead." if do_spcc else ""),
+            LogColor.GREEN,
+        )
 
-    def clean_up(self, prefix):
+    def clean_up(self, prefix=None):
         """Cleans up all files in the process directory."""
         self.siril.cmd("cd", "../")
-        process_directory = os.path.join(self.current_working_directory, 'process')
-        if os.path.isdir(process_directory):
-            shutil.rmtree(process_directory)
-
-
-    
-
+        # TODO: Delete each previous sequence to save space
+        # process_directory = os.path.join(self.current_working_directory, 'process')
+        # if os.path.isdir(process_directory):
+        #     shutil.rmtree(process_directory)
 
     def create_widgets(self):
         """Creates the UI widgets."""
@@ -265,16 +282,17 @@ class PreprocessingInterface:
             text=f"{APP_NAME} v{VERSION}",
             style="Bold.TLabel",
         ).pack(anchor="w")
-        ttk.Label(main_frame, text=f"Author: {AUTHOR}", style="Bold.TLabel" ).pack(
+        ttk.Label(main_frame, text=f"Author: {AUTHOR}", style="Bold.TLabel").pack(
             anchor="w", pady=(0, 0)
         )
-        ttk.Label(
-            main_frame, text=f"Website: {WEBSITE}", style="Bold.TLabel"
-        ).pack(anchor="w", pady=(0, 0))
-
-        ttk.Label(main_frame, text=f"Current Working Directory: {self.current_working_directory}").pack(
-            anchor="w", pady=(0, 10)
+        ttk.Label(main_frame, text=f"Website: {WEBSITE}", style="Bold.TLabel").pack(
+            anchor="w", pady=(0, 0)
         )
+
+        ttk.Label(
+            main_frame,
+            text=f"Current Working Directory: {self.current_working_directory}",
+        ).pack(anchor="w", pady=(0, 10))
 
         # Telescope section
         telescope_section = ttk.LabelFrame(main_frame, text="Telescope", padding=10)
@@ -291,16 +309,14 @@ class PreprocessingInterface:
         # Mode to handle >2048 files
         telescope_section.pack(fill=tk.X, pady=5)
 
-        ttk.Label(
-            telescope_section, text="2048+ Files:", style="Bold.TLabel"
-        ).grid(row=1, column=0, sticky="w")
+        ttk.Label(telescope_section, text="2048+ Files:", style="Bold.TLabel").grid(
+            row=1, column=0, sticky="w"
+        )
 
         fitseq_checkbox_variable = tk.BooleanVar()
 
         fitseq_checkbox = ttk.Checkbutton(
-            telescope_section,
-            text="Enable",
-            variable=fitseq_checkbox_variable
+            telescope_section, text="Enable", variable=fitseq_checkbox_variable
         )
         fitseq_checkbox.grid(row=1, column=1, sticky="w")
 
@@ -308,19 +324,14 @@ class PreprocessingInterface:
         tksiril.create_tooltip(
             fitseq_checkbox,
             "Enable this option if you have more than 2048 images to process. A different workflow will be used and the sequence will not be plate solved and the framing method will be default."
-            "This can affect how large mosaic sessions look."
+            "This can affect how large mosaic sessions look.",
         )
 
-
-
         # Optional Preprocessing Steps
-
-        # Optional Preprocessing Steps 
         calib_section = ttk.LabelFrame(
             main_frame, text="Optional Preprocessing Steps", padding=10
         )
 
-        # Regular BG Extraction
         calib_section.pack(fill=tk.X, pady=5)
         ttk.Label(
             calib_section, text="Background Extraction:", style="Bold.TLabel"
@@ -332,22 +343,7 @@ class PreprocessingInterface:
             text="",
             variable=bg_extract_checkbox_variable,
         ).grid(row=2, column=1, sticky="w")
-        
-        # Graxpert BG Extraction
-        # calib_section.pack(fill=tk.X, pady=5)
-        # ttk.Label(
-        #     calib_section, text="Graxpert BG Extraction:", style="Bold.TLabel"
-        # ).grid(row=2, column=0, sticky="w")
 
-        # graxpert_checkbox_variable = tk.IntVar()
-        # ttk.Checkbutton(
-        #     calib_section,
-        #     text="Background Extraction?",
-        #     variable=graxpert_checkbox_variable,
-        # ).grid(row=2, column=1, sticky="w")
-
-        # Registering Frames
-        calib_section.pack(fill=tk.X, pady=5)
         ttk.Label(calib_section, text="Registration:", style="Bold.TLabel").grid(
             row=3, column=0, sticky="w"
         )
@@ -397,9 +393,6 @@ class PreprocessingInterface:
             ),
         )
 
-        # Stacking Frames
-
-        calib_section.pack(fill=tk.X, pady=5)
         ttk.Label(calib_section, text="Stacking:", style="Bold.TLabel").grid(
             row=5, column=0, sticky="w"
         )
@@ -471,18 +464,15 @@ class PreprocessingInterface:
 
         if self.gaia_catalogue_available:
             ttk.Label(
-                spcc_section, 
-                text="✓ Local Gaia Available", 
-                foreground="green", 
-                style="Success.TLabel"
+                spcc_section,
+                text="✓ Local Gaia Available",
+                foreground="green",
+                style="Success.TLabel",
             ).grid(row=2, column=2, sticky="w")
         else:
             ttk.Label(
-                spcc_section, 
-                text="✗ Local Gaia Not available", 
-                foreground="red"
+                spcc_section, text="✗ Local Gaia Not available", foreground="red"
             ).grid(row=2, column=2, sticky="w")
-        
 
         # Run button
         ttk.Button(
@@ -507,7 +497,6 @@ class PreprocessingInterface:
             ),
         ).pack(pady=(15, 0), anchor="w")
 
-
         # SPCC Button for Debugging
         # ttk.Button(
         #     main_frame,
@@ -524,7 +513,7 @@ class PreprocessingInterface:
         # ttk.Button(
         #     main_frame,
         #     text="FITS Extract",
-        #     command=lambda: self.extract_fits(), 
+        #     command=lambda: self.extract_fits(),
         # ).pack(pady=(15, 0), anchor="w")
 
     def close_dialog(self):
@@ -560,7 +549,7 @@ class PreprocessingInterface:
         if use_bias:
             self.convert_files("biases")
 
-        # TODO: Calibration frames processing 
+        # TODO: Calibration frames processing
 
         seq_name = "lights" if fitseq_mode else "lights_"
         # if graxpert:
@@ -569,7 +558,7 @@ class PreprocessingInterface:
         if bg_extract:
             self.seq_bg_extract()
             seq_name = "bkg_" + seq_name
-        
+
         # Don't plate solve if mosaic mode on, doesn't do anything but waste time
         if not self.fitseq_mode:
             self.seq_plate_solve(seq_name=seq_name)
@@ -588,9 +577,15 @@ class PreprocessingInterface:
         self.save_image("_og")
 
         if do_spcc:
-            self.spcc(oscsensor=telescope, filter=filter, catalog=catalog, whiteref="Average Spiral Galaxy")
-        
-        self.autostretch()
+            self.spcc(
+                oscsensor=telescope,
+                filter=filter,
+                catalog=catalog,
+                whiteref="Average Spiral Galaxy",
+            )
+
+        self.autostretch(do_spcc=do_spcc)
+        self.clean_up()
         self.close_dialog()
 
 
